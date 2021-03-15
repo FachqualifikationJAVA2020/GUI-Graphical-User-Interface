f6d4a4e88c527d3168c0de1094ff02753c4fa307
VanKhaos <xv50k870!?>
2021-03-15 10:33:38 +0100

    init

---
+++ src/einfach/MeineErsteGui.java
@@ -0,0 +1,11 @@
+package einfach;
+
+/*
+ *  Swing ist das am weitestens verbreitete Java-GUI-Paket
+ *  (nicht das neuste)
+ *  Alle Swing.Klassen fangen mit einem "J" an
+ */
+import javax.swing.*;
+
+public class MeineErsteGui extends JFrame {
+}
