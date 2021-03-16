7e6655078bcc542efc7cd1f3c8230d68028f9a7d
VanKhaos <xv50k870!?>
2021-03-16 09:06:15 +0100

    add FlowLayout

--- src/layout/Flow.java
+++ src/layout/Flow.java
@@ -3,6 +3,7 @@ package layout;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Flow {
     /*
      *  Das FlowLayout erlaubt mehrere Elemente nebeneinander anzuordnen
@@ -20,12 +21,19 @@ public class Flow {
         eingabe = new JTextField(10);
         button = new JButton("Klick");
 
+        //  Layout festlegen
         frame.setLayout(new FlowLayout());
 
+        //  Komponenten hinzufügen
         frame.add(label);
         frame.add(eingabe);
         frame.add(button);
 
+        //  MINIMALE Fenster Größe
+        frame.setMinimumSize(new Dimension(200,500));
+
+        //  Wichtige Eigenschaften
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
